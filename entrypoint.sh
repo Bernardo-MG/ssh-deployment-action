c8fc660a08c9cc773996ee8d8fa322a4c3fe689a
Bernardo <programming@bernardomg.com>
2022-02-24 23:47:46 +0100

    Registers host

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,16 @@ host=${3}
 port=${4}
 path=${5}
 
+# Install dependencies
+apk update;
+apk add openssh;
+
+# Makes sure the SSH folder exists
+mkdir -p ~/.ssh/;
+
+# Registers host
+ssh-keyscan -H ${host} >> ~/.ssh/known_hosts;
+
 # Deploy
 sshpass -p "${password}" scp -o StrictHostKeyChecking=no -p ${port} -r ./* ${username}'@'${host}':'${path};
 
