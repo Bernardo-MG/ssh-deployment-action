131de252dc20929113c808acfb71427bfc2b82bb
Bernardo-MG <programming@bernardomg.com>
2022-11-22 11:10:58 +0100

    Added comments to script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
+
 #
-# Function for deploying the current folder through SSH
+# Deploys folder contents through SSH.
+#
+# -- PARAMETERS --
+#
+# - $1: string, username for deployment
+# - $2: string, password for deployment
+# - $3: string, host where the folder will be deployed
+# - $4: string, host port for deployment
+# - $5: string, path inside the host where the files will be deployed
+# - $6: string, local folder containing the files to deploy
+#
+# -- EFFECTS --
+#
+# - The folder contents get deployed at the host
+#
 
 # Fails if any commands returns a non-zero value
 set -e
 
+# Read input parameters
 username=${1}
 password=${2}
 host=${3}
@@ -12,6 +28,7 @@ port=${4}
 path=${5}
 folder=${6}
 
+# Move to the folder to deploy
 cd ${folder}
 
 # Deploy
